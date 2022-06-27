7f3147d01c72f19524dc7dd84bce828c08dda8c8
Ahkar Toe Maw <80157174+ahkar33@users.noreply.github.com>
2022-06-27 22:01:58 +0630

    finished update

--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -3,21 +3,38 @@ const app = Vue.createApp({
     data() {
         return {
             users: [],
+            userId: '',
             userName: '',
             userAddress: '',
-            isOpen: false
+            editUser: null
         }
     },
     methods: {
         postUser(event) {
             event.preventDefault();
-            let data = { userName: this.userName, userAddress: this.userAddress };
-            axios
-                .post('http://localhost:8080/addUser', data)
-                .then(() => this.getData())
-                .catch(error => console.log(error));
-            this.userName = "";
-            this.userAddress = "";
+            let name = this.userName.replace(/\s/g, "");
+            let address = this.userAddress.replace(/\s/g, "");
+            if (name.length > 0 && address.length > 0) {
+                if (this.editUser == null) {
+                    let data = { userName: this.userName, userAddress: this.userAddress };
+                    axios
+                        .post('http://localhost:8080/addUser', data)
+                        .then(() => this.getData())
+                        .catch(error => console.log(error));
+                    this.userName = "";
+                    this.userAddress = "";
+                } else {
+                    let data = { userName: this.userName, userAddress: this.userAddress };
+                    let userId = this.userId;
+                    axios
+                        .put(`http://localhost:8080/updateUser/${userId}/`, data)
+                        .then(() => this.getData())
+                        .catch(error => console.log(error));
+                    this.editUser = null;
+                    this.userName = "";
+                    this.userAddress = "";
+                }
+            }
         },
         deleteUser(event, userId) {
             event.preventDefault();
@@ -26,17 +43,12 @@ const app = Vue.createApp({
                 .then(() => this.getData())
                 .catch(error => console.log(error));
         },
-        updateUser(event, userId) {
+        updateUser(event, index) {
             event.preventDefault();
-            console.log(userId)
-            let data = { userName: this.userName, userAddress: this.userAddress };
-            axios
-                .put(`http://localhost:8080/updateUser/${userId}/`, data)
-                .then(() => this.getData())
-                .catch(error => console.log(error));
-        },
-        openUpdate() {
-            this.isOpen = !this.isOpen;
+            this.editUser = index;
+            this.userId = this.users[index].userId;
+            this.userName = this.users[index].userName;
+            this.userAddress = this.users[index].userAddress;
         },
         getData() {
             axios
